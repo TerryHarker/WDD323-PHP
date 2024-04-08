c69e9d303339d969729fb24dd4bfc821aef6fc07
Terry Harker PHP <terry.harker@bytekultur.net>
2024-04-08 20:36:15 +0200

    Update: User IP, User Agent und Logout Funktion

--- Woche4/geschuetzter-bereich.php
+++ Woche4/geschuetzter-bereich.php
@@ -3,40 +3,64 @@
 session_name( md5('MEINEIGENERSESSIONNAME') ); 
 session_start(); // session Zugriff gewähren - erst nach session_name, aber vor dem ersten Session Zugriff!
 
-$session_lifetime = 10; // sekunden
-$isLoggedIn = false; 
+$session_lifetime = 10 * 60; // sekunden
+$isLoggedIn = false; // Status Variable für die Entscheidung, ob der Adminbereich besucht werden darf oder nicht
 
 echo '<pre>';
 echo 'SESSION: ';
 print_r($_SESSION);
 echo '</pre>';
 
+
+
+
 // Prüfen
 if( isset($_SESSION['isloggedin']) &&  $_SESSION['isloggedin'] === true){
     // user ist eingeloggt
     $isLoggedIn = true;
-    
-    // IP Prüfen (TODO)
 
-    // User Agent Prüfen (TODO)
+    // Logout-Mitteilung in URL prüfen - ist der GET Parameter vorhanden?
+    if( isset($_GET['logout']) && $_GET['logout'] == true ){
+        // Vorhanden: user will sich ausloggen
+        $isLoggedIn = false;
+    }
+    
+    // IP Prüfen - gespeicherte IP mit aktueller IP vergleichen
+    if( $_SESSION['userip'] != $_SERVER['REMOTE_ADDR'] ){
+        $isLoggedIn = false;
+    }
+
+    // User Agent Prüfen  - gespeicherte User Agent mit aktuellem vergleichen
+    if( $_SESSION['useragent'] != $_SERVER['HTTP_USER_AGENT']){
+        $isLoggedIn = false;
+    }
 
     // Zeit einschränken:
-    $zeitjetzt = time(); // Vergangene Sekunden seit letztem Zeitstempel
+    $zeitjetzt = time(); // Vergangene Sekunden seit letzter Aktivität / letztem Page load
     if( $zeitjetzt-$_SESSION['timestamp'] > $session_lifetime ){
-        $isLoggedIn = false; // Session zu alt!
+        $isLoggedIn = false; // Zu lange inaktiv
     }
 
 }
 
 
 if($isLoggedIn === false){
-    // user darf nicht zugreifen - zum Formular umleiten
+    // user darf nicht zugreifen
+    
+    // Session zurücketzen, indem die gespeicherten Informationen gelöscht werden - logout:
+    unset( $_SESSION['isloggedin'] );
+    unset( $_SESSION['userip'] );
+    unset( $_SESSION['useragent'] );
+    unset( $_SESSION['timestamp'] );
+    
+    // zum Formular umleiten
     header("location: login-formular.php");
     exit;
 }
 
 // neue session ID für den nächsten Aufruf - so wird eine eventuell geklaute Session ID bei jedem neuen page load ungültig
-session_regenerate_id(); 
+session_regenerate_id();
+$_SESSION['timestamp'] = time(); // timestamp erneuern bei jedem page load (NACH allen Überprüfungungen!)
 ?>
 <!DOCTYPE html>
 <html>
@@ -107,7 +131,7 @@ session_regenerate_id();
             <strong>Adminbereich</strong>
         </div>
         <div class="inner flex-right">
-            <a class="button">Logout</a>
+            <a class="button" href="geschuetzter-bereich.php?logout=true">Logout</a>
         </div>
     </div>
     <div class="container-lg">
